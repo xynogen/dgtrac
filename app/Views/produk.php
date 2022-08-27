0148e9b029481257a1ff57acd89efda5666c4613
Muhammad Fikri <fikri3142857@gmail.com>
2022-08-27 07:14:32 +0700

    fix(produk)

--- app/Views/produk.php
+++ app/Views/produk.php
@@ -2,37 +2,37 @@
 
 <body>
     <?= $this->include("navbarHome") ?>
-    <?= $this->include("carousel") ?>
+    <?= $this->include("carouselHome") ?>
 
-    <div class="container" style="padding: 0;">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="row">
-                    
-                </div>
-
+    <div class="container">
+        <div class="row" style="margin-bottom: 15%;">
+            <div class="col-md-12 justify-content-center">
                 <div class="row mt-3">
-                    <div class="d-flex flex-row justify-content-around" id="btn-buy">
-                        <div class="flex-column">
+
+                    <div class="col-md-6">
+                        <div class="col-12">
                             <h3 class="display-6 mx-4 text-center">
                                 <?= $banners[0]["name"] ?>
                             </h3>
-                            <h3 class="fs-5 mx-4">
+                            <h3 class="fs-5 mx-4 text-center">
                                 Rp. <?= $banners[0]["harga"] ?>,-/<?= $banners[0]["satuan"] ?>
                             </h3>
                         </div>
-                        <div class="mt-3 mb-5">
-                            <a href="<?= $banners[0]['lokasi'] ?>" target="_blank" class="btn btn-dark"><i class="bi bi-geo-alt"></i></a>
-                            <a href="<?= $banners[0]['kontak'] ?>" class="btn btn-dark"><i class="bi bi-person-lines-fill"></i></a>
-                        </div>
                     </div>
 
-                    
+                    <div class="col-md-6">
+                        <div class="mt-3 mb-5 d-flex justify-content-center">
+                            <div style="margin-right: 10px;">
+                                <a href="<?= $banners[0]['lokasi'] ?>" target="_blank" class="btn btn-dark"><i class="bi bi-geo-alt"></i></a>
+                            </div>
+                            <a href="<?= $banners[0]['kontak'] ?>" class="btn btn-dark"><i class="bi bi-person-lines-fill"></i></a>
+                        </div>
+                    </div>    
                 </div>
             </div>
         </div>
     </div>
-    <footer class="mt-5 py-5 bg-dark my-footer">
+    <footer class="py-4 bg-dark my-footer">
         <div class="container">
             <div class="col-12">
                 <p class="m-0 text-center text-white">Copyright© 2022 DG-Trac</p>
