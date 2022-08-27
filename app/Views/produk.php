f0fd77f4a3455ec090702c1c8fe9c5c548073709
Muhammad Fikri <fikri3142857@gmail.com>
2022-08-27 18:37:07 +0700

    add(mitra card)

--- app/Views/produk.php
+++ app/Views/produk.php
@@ -8,7 +8,6 @@
         <div class="row" style="margin-bottom: 15%;">
             <div class="col-md-12 justify-content-center">
                 <div class="row mt-3">
-
                     <div class="col-md-6">
                         <div class="col-12">
                             <h3 class="display-6 mx-4 text-center">
@@ -27,8 +26,18 @@
                             </div>
                             <a href="<?= $banners[0]['kontak'] ?>" class="btn btn-dark"><i class="bi bi-person-lines-fill"></i></a>
                         </div>
-                    </div>    
+                    </div>
                 </div>
+                <div class="row">
+                    <div class="card mx-auto" style="width: 18rem; box-shadow: 1px 1px 10px rgb(77, 77, 77);">
+                        <img src="/assets/img/<?= $banners[0]['logo'] ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $banners[0]["nama"] ?><i class="bi bi-check-circle-fill text-primary"></i></h5>
+                        </div>
+                        <a href="/mitra/<?= $banners[0]['id']?>" class="stretched-link"></a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
