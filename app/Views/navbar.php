c07f1c02790e54c92797d07ddaaf344be23d2612
Muhammad Fikri <fikri3142857@gmail.com>
2022-09-19 11:45:44 +0700

    center the navbar and add google search console

--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="/">
       <img id="my-logo" src="<?php echo base_url('assets/icon/icon-white.png'); ?>" width="90" height="20" />
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="bi bi-list my-text"></i>
+      <i class="bi bi-list my-text"></i>
     </button>
-    
+
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link my-text" href="/">Home</a>
@@ -17,5 +17,3 @@
     </div>
   </div>
 </nav>
-
-
--- app/Views/navbarHome.php
+++ app/Views/navbarHome.php
@@ -1,30 +1,20 @@
 <nav class="navbar navbar-expand-lg my-navbar ">
   <div class="container">
-
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="/">
       <img id="my-logo" src="<?php echo base_url('assets/icon/icon-white.png'); ?>" width="90" height="20" />
     </a>
-
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <i class="bi bi-list my-text"></i>
     </button>
 
-    <div class="collapse navbar-collapse my-navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link my-text" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link my-text" href="/mitra">Mitra</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link my-text" href="/produk">Produk</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link my-text" href="/tentang">Tentang Kami</a>
-        </li>
-      </ul>
-
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link my-text" href="/">Home</a>
+        <a class="nav-link my-text" href="/mitra">Mitra</a>
+        <a class="nav-link my-text" href="/produk">Produk</a>
+        <a class="nav-link my-text" href="/tentang">Tentang Kami</a>
+      </div>
     </div>
+
   </div>
 </nav>
